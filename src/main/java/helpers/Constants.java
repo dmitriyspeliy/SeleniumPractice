374b64d73208c1bcf6c143f338f1896791ecbead
dmitryspeliy <dmitriypospelov93@gmail.com>
2022-05-18 16:33:23 +0700

    First

--- src/main/java/helpers/Constants.java
+++ src/main/java/helpers/Constants.java
@@ -1,6 +1,6 @@
 package helpers;
 
 public class Constants {
-    public static int DEFAULT_TIMEOUT = 30;
+    public static int DEFAULT_TIMEOUT = 0;
     public static int DEFAULT_TIMEOUT_EX = 20;
 }
